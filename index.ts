c6c2948f02f37440809e06f638e2a66c4e47ffa1
Juan Carlos Burroughs <carlosburroughs@gmail.com>
2026-06-15 16:44:27 -0400

    feat: configure UI repo as publishable npm package

---
+++ index.ts
@@ -0,0 +1,36 @@
+// Sections
+export * from "./components/sections/about-section";
+export * from "./components/sections/campaign-timeline-section";
+export * from "./components/sections/get-involved-section";
+export * from "./components/sections/hero-section";
+export * from "./components/sections/site-footer";
+export * from "./components/sections/site-nav";
+
+// Components
+export * from "./components/content-block-renderer";
+export * from "./components/election-countdown";
+export * from "./components/endorsement-card";
+export * from "./components/fade-in";
+export * from "./components/involvement-card";
+export * from "./components/sign-request-dialog";
+export * from "./components/status-tracker";
+export * from "./components/theme-provider";
+export * from "./components/volunteer-dialog";
+
+// Pages
+export * from "./pages/full-site";
+export * from "./pages/construction";
+
+// Primitives
+export * from "./primitives/avatar";
+export * from "./primitives/badge";
+export * from "./primitives/button";
+export * from "./primitives/card";
+export * from "./primitives/carousel";
+export * from "./primitives/dialog";
+export * from "./primitives/input";
+export * from "./primitives/label";
+export * from "./primitives/popover";
+export * from "./primitives/progress";
+export * from "./primitives/separator";
+export * from "./primitives/textarea";
